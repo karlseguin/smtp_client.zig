549cc8a2ceece477695b5a4fbe889cdc24a8fe12
Arshid <arshidkv12@gmail.com>
2024-01-30 07:51:07 +0530

    Update build.zig

--- build.zig
+++ build.zig
@@ -1,45 +1,47 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
-	const target = b.standardTargetOptions(.{});
-	const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-	const smtp_module = b.addModule("smtp_client", .{
-		.source_file = .{ .path = "src/smtp.zig" },
-	});
+    {
+        // example
+        const exe = b.addExecutable(.{
+            .name = "smtp_client_demo",
+            .root_source_file = .{ .path = "example/main.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
+        const smtp_client = b.dependency("smtp_client", .{
+            .target = target,
+            .optimize = optimize,
+        });
+        exe.addModule("smtp_client", smtp_client.module("smtp_client"));
 
-	{
-		// example
-		const exe = b.addExecutable(.{
-			.name = "smtp_client_demo",
-			.root_source_file = .{ .path = "example/main.zig" },
-			.target = target,
-			.optimize = optimize,
-		});
-		exe.root_module.addImport("smtp_client", smtp_module);
-		b.installArtifact(exe);
+        // exe.root_module.addImport("smtp_client", smtp_module);
+        b.installArtifact(exe);
 
-		const run_cmd = b.addRunArtifact(exe);
-		run_cmd.step.dependOn(b.getInstallStep());
-		if (b.args) |args| {
-			run_cmd.addArgs(args);
-		}
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
 
-		const run_step = b.step("run", "Run the example");
-		run_step.dependOn(&run_cmd.step);
-	}
+        const run_step = b.step("run", "Run the example");
+        run_step.dependOn(&run_cmd.step);
+    }
 
-	{
-		// tests
-		const lib_test = b.addTest(.{
-			.root_source_file = .{ .path = "src/smtp.zig" },
-			.target = target,
-			.optimize = optimize,
-		});
-		const run_test = b.addRunArtifact(lib_test);
-		run_test.has_side_effects = true;
+    {
+        // tests
+        const lib_test = b.addTest(.{
+            .root_source_file = .{ .path = "src/smtp.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
+        const run_test = b.addRunArtifact(lib_test);
+        run_test.has_side_effects = true;
 
-		const test_step = b.step("test", "Run tests");
-		test_step.dependOn(&run_test.step);
-	}
+        const test_step = b.step("test", "Run tests");
+        test_step.dependOn(&run_test.step);
+    }
 }
